8a7c4b9fc524f3c1fa24817b5b55ceffb7cf485e
jwick4298 <jwick4298@gmail.com>
2021-03-17 18:57:31 +0530

    [Add] Restricted views for 'admin' access only.

--- Myshop/views.py
+++ Myshop/views.py
@@ -1,4 +1,5 @@
-from django.contrib.auth.decorators import login_required
+from django.contrib.auth.decorators import user_passes_test
+from django.contrib.auth.mixins import UserPassesTestMixin
 from django.http import JsonResponse
 from django.shortcuts import render, redirect, get_object_or_404
 from django.urls import reverse
@@ -20,12 +21,16 @@ class ProductDetail(DetailView):
     template_name = 'product_details.html'
 
 
-@method_decorator(login_required, name='dispatch')
-class ProductUpdate(UpdateView):
+
+class ProductUpdate(UserPassesTestMixin, UpdateView):
     model = Product
     template_name = 'product_update.html'
     form_class = ProductForm
     context_object_name = 'product'
+
+    # this func for protecting the view for 'admin' access only, and to use it inherit UserPassesTestMixin at the leftmost position of your view.
+    def test_func(self):
+        return self.request.user.username.startswith('admin')
 
     def get_object(self):
         pk = self.kwargs.get('pk')
@@ -45,7 +50,12 @@ class ProductUpdate(UpdateView):
 
 # IMAGE_FILE_TYPES = ['png', 'jpg', 'jpeg']
 
-@login_required
+# def admin_check(user): 
+#     return user.username.startswith('admin')
+
+# @user_passes_test(admin_check)
+# ----------- Alternative Code of admin check method above ------------------------
+@user_passes_test(lambda user: user.is_superuser)
 def add_product(request):
     form = ProductForm()
     if request.method == 'POST':
